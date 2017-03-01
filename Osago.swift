b8f7940b4355d4fcb0479dd239ac05e0836224d9
AlexeyKrestinin <alexeykrestinin@gmail.com>
2017-03-01 13:57:26 +0300

    Complete layouts

---
+++ Osago.swift
@@ -0,0 +1,9 @@
+//
+//  Osago.swift
+//  Program2Lesson1Krestinin
+//
+//  Created by Алексей Крестинин on 01.03.17.
+//  Copyright © 2017 Alexey Krestinin. All rights reserved.
+//
+
+import Foundation
---
+++ OsagoView.swift
@@ -0,0 +1,21 @@
+//
+//  OsagoView.swift
+//  Program2Lesson1Krestinin
+//
+//  Created by Алексей Крестинин on 01.03.17.
+//  Copyright © 2017 Alexey Krestinin. All rights reserved.
+//
+
+import UIKit
+
+class OsagoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
