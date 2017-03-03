0674c0686d507efaf3283f8f270294663de78162
AlexeyKrestinin <alexeykrestinin@gmail.com>
2017-03-03 15:46:47 +0300

    full complete

--- Osago.swift
+++ Osago.swift
@@ -7,3 +7,57 @@
 //
 
 import Foundation
+
+
+struct Calculate {
+    
+    var power:Double
+    var age:Double
+//    var powerPrecision:Double
+//    var agePrecision:Double
+    var baseSum:Double
+    var cityPrecision:Double
+    var expirience:Int
+    
+    func agePrecision () -> Double {
+        switch (age,expirience) {
+        case (18...22,0...3):
+            return 1.8
+        case (22...120,0...3):
+            return 1.7
+        case (18...22,3...100):
+            return 1.6
+        case (22...120,3...100):
+            return 1.0
+        default:
+            return 0
+        }
+    }
+    
+    func powerPrecision () -> Double {
+        switch power {
+        case 49...50:
+            return 0.6
+        case 51...70:
+            return 1.0
+        case 71...100:
+            return 1.1
+        case 101...120:
+            return 1.2
+        case 121...150:
+            return 1.4
+        case 151...300:
+            return 1.6
+        default:
+            return 0
+        }
+   
+    }
+
+    func osagoPrice () -> Double {
+        let aP:Double = agePrecision()
+        let pP:Double = powerPrecision()
+        return (baseSum * cityPrecision * aP * pP)
+    }
+    
+}
--- OsagoView.swift
+++ OsagoView.swift
@@ -10,12 +10,61 @@ import UIKit
 
 class OsagoView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    var calculator = Calculate(power: 0, age: 0, baseSum: 3432, cityPrecision: 2,expirience: 0)
+    
+    // LABELS
+    @IBOutlet var agePrecisionLabel: UILabel!
+    
+    @IBOutlet var sumLabel: UILabel!
+    
+    @IBOutlet var powerPrecisionLabel: UILabel!
+    
+    @IBOutlet var currentAgeLabel: UILabel!
+    
+    @IBOutlet var currentPowerLabel: UILabel!
+    
+    
+    //SLIDERS
+    @IBOutlet var ageSlider: UISlider!
+    
+    @IBOutlet var powerSlider: UISlider!
+    
+    
+    // FUNC
+    
+    @IBAction func expirienceChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: calculator.expirience = 2
+        case 1: calculator.expirience = 4
+        default:
+            fatalError("что то пошло не так в func experience")
+        }
 
+    }
+    
+    
+    @IBAction func ageSliderChange(_ sender: UISlider) {
+        calculator.age = Double(ageSlider.value)
+        agePrecisionLabel.text = "Коэффициент возраста:\(calculator.agePrecision())"
+        currentAgeLabel.text = "\(Int(ageSlider.value))"
+    }
+  
+    
+    
+    @IBAction func powerSliderChange(_ sender: UISlider) {
+        calculator.power = Double(powerSlider.value)
+        powerPrecisionLabel.text = "Коэффициент мощности: \(calculator.powerPrecision())"
+        currentPowerLabel.text = "\(Int(powerSlider.value))"
+        
+     sumLabel.text = "ИТОГО: \(calculator.osagoPrice()) руб."
+        
+    }
+  
+
+   
 }
+
+
+
+
+
